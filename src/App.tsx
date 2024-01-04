bc4f1775aa02f8c660c6968fc5b37ff329389c67
nirajgiriXD <nirajgiri9860@googlemail.com>
2024-01-04 15:11:02 +0545

    Sort the tags

--- src/App.tsx
+++ src/App.tsx
@@ -22,12 +22,14 @@ const App = () => {
           "https://raw.githubusercontent.com/nirajgiriXD/garden-of-quotes/main/public/quotes.json"
         );
         const data = await response.json();
-        const setOfTags = new Set<string>(
+        let setOfTags = new Set<string>(
           data.data.flatMap((quote: QuoteItemProp) => quote.tags)
         );
 
+        setOfTags.add("any");
+        setOfTags = new Set<string>(Array.from(setOfTags).sort());
+
         setQuotes(data.data);
-        setOfTags.add("any")
         setTags(setOfTags);
       } catch (error) {
         console.error("Error fetching data:", error);
