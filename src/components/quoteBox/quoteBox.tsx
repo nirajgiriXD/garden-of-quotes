ce82144a9b6b37d9ff1ca6fde72a1ca758d936ee
sumanjha108155 <suman.jha108155@gmail.com>
2024-01-04 14:06:44 +0530

    fix: no responsive ui
    the unresponsive ui has been fixed with some other minute changes on the
    ui. the dropdown section has been moved to the bottom , while the tags
    section has been moved to the top
    
    Closes #36

--- src/components/quoteBox/quoteBox.tsx
+++ src/components/quoteBox/quoteBox.tsx
@@ -1,3 +1,5 @@
+
+
 /**
  * External dependencies.
  */
@@ -47,68 +49,58 @@ const QuoteBox: React.FC<QuoteBoxProp> = ({ quotes, tags }) => {
 
   return (
     <div className="main-container">
-      <div className="display-container p-5">
-        <div className="row filter-container  mb-2 gx-0">
-          <div className="col-12 d-flex justify-content-end align-items-center ">
-            <div className="row ">
-              <div className="col-12 h-100 d-flex justify-content-end align-items-center">
-                <Dropdown onSelect={handleDropdownChange}>
-                  <Dropdown.Toggle variant="light">
-                    {selectedTag[0].toUpperCase() + selectedTag.substring(1)}
-                  </Dropdown.Toggle>
-                  <Dropdown.Menu>
-                    {Array.from(tags).map((tag) => {
-                      return (
-                        <Dropdown.Item eventKey={tag} key={tag}>
-                          {tag[0].toUpperCase() + tag.substring(1)}
-                        </Dropdown.Item>
-                      );
-                    })}
-                  </Dropdown.Menu>
-                </Dropdown>
-              </div>
-            </div>
-          </div>
-        </div>
-
-        <div className="row message-container mb-2 gx-0">
-          <div className="col-12">
-            <div className="quote-text">
-              <i className="fa fa-quote-left"></i>
-              <span>{quoteItem.quote}</span>
-              <i className="fa fa-quote-right"></i>
-            </div>
-            <div className="quote-author pt-3">
-              <span>
-                - {quoteItem.author === "" ? "Someone" : quoteItem.author}
-              </span>
-            </div>
-          </div>
-        </div>
-        <div className="row footer-container mb-2 gx-0">
-          <div className="col-12 d-flex align-items-center">
-            <div className="row w-100">
-              <hr />
-              <div className="col-6">
-                <span className="quote-tags">
-                  Tags:{" "}
-                  {quoteItem.tags.length === 0
+        <div className="display-container p-5">
+            <div className="row border-bottom border-dark border-2">
+                <div className="col-auto d-flex justify-content-start align-items-center">
+                    <p>Tags:{" "}
+                    {quoteItem.tags.length === 0
                     ? "general"
-                    : quoteItem.tags.join(", ")}
-                </span>
-              </div>
-              <div className="col-6 d-flex justify-content-end">
-                <button
-                  className="btn btn-danger"
-                  onClick={generateRandomQuote}
-                >
-                  New quote
-                </button>
-              </div>
+                    : quoteItem.tags.join(", ")}</p>
+                </div>
             </div>
-          </div>
+
+            <div className="row mt-5 message-container">
+                <div className="col">
+                    <div className="row">
+                        <div className="col-auto">
+                            <p className="p-1 fs-5 fw-bolder">
+                                <i className="fa fa-quote-left"></i>
+                                <span className="ms-1 me-1">{quoteItem.quote}</span>
+                                <i className="fa fa-quote-right"></i>
+                            </p>
+                        </div>
+                    </div>
+                    
+                    <div className="row">
+                        <div className="col-auto d-flex justify-content-end align-items-center">
+                            <span className="ms-auto p-1">- {quoteItem.author === "" ? "Someone" : quoteItem.author}</span>
+                        </div>
+                    </div>
+                </div>
+            </div>
+
+            <div className="row d-flex justify-content-between align-items-center mt-5">
+                <div className="col-auto">
+                        <Dropdown onSelect={handleDropdownChange} drop="up" className="mb-2">
+                        <Dropdown.Toggle variant="light">
+                            {selectedTag[0].toUpperCase() + selectedTag.substring(1)}
+                        </Dropdown.Toggle>
+                        <Dropdown.Menu>
+                            {Array.from(tags).map((tag) => {
+                            return (
+                            <Dropdown.Item eventKey={tag} key={tag}>
+                                {tag[0].toUpperCase() + tag.substring(1)}
+                            </Dropdown.Item>
+                            );
+                            })}
+                        </Dropdown.Menu>
+                    </Dropdown>
+                </div>
+                <div className="col-auto">
+                    <button className="btn btn-danger  mb-2" onClick={generateRandomQuote}>New quote</button>
+                </div>
+            </div>
         </div>
-      </div>
     </div>
   );
 };
